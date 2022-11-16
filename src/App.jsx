72fd2d196def4581ebf467127158b7775ddebdbb
Jonas Bechler <jonasbechler@gmail.com>
2022-11-16 12:23:11 +0100

    rm submodule

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import './App.css'
 import Main from './SWK_React_Template/src/components/Main/Main';
 
 function App() {
-	
+	const name = "";
+	const image_path = "";
 	return (
 
 		<Main config={config}></Main>
