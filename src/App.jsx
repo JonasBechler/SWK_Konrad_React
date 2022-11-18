3c86d2f3c1ab2b49d5197f17f025cbab9a284562
Jonas Bechler <jonasbechler@gmail.com>
2022-11-18 12:41:34 +0100

    name changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,8 @@ import './SWK_React_Template/src/styles/App.css'
 import Main from './SWK_React_Template/src/components/Main/Main';
 
 function App() {
-	const name = "Name";
-	//const icon = require("./Bicycle.png");
-	const icon = require("./Logo_Placeholder.png");
+	const name = "Konrad";
+	const icon = require("./Bicycle.png");
 	config.port = config.konrad.port
 	config.port_react = config.konrad.port_react
 
